d2fc5b8c331b0b6a820406e0d0eef834d8f60f1a
ученик <dimaming2315@yandex.ru>
2021-02-08 18:17:58 +0500

    add map label

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
-
+import requests
 
 from PyQt5.Qt import QMainWindow, QApplication
 from PyQt5 import uic
+from io import BytesIO
+from PIL import Image
+from PIL.ImageQt import ImageQt
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QPixmap
 
 
 class AppMainWindow(QMainWindow):
@@ -15,10 +20,30 @@ class AppMainWindow(QMainWindow):
         self.server = "http://static-maps.yandex.ru/1.x/"
 
     def show_map(self):
+        is_coords_right = True
         try:
             coords = [float(self.lineEdit.text()), float(self.lineEdit_2.text())]
         except:
             self.statusBar().showMessage("Неверный формат координат", 1000)
+            is_coords_right = False
+
+        if is_coords_right:
+            map_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "ll": ",".join([str(i) for i in coords]),
+                "l": "map",
+                "spn": ",".join([str(i) for i in [0.01, 0.01]]),
+                "size": ",".join([str(i) for i in [450, 450]])
+            }
+            response = requests.get(self.server, params=map_params)
+            print(response)
+            print(response.headers)
+            print(response.url)
+            if response:
+                map = Image.open(BytesIO(response.content))
+                self.qimg = ImageQt(map)
+                self.pixmap = QPixmap.fromImage(self.qimg).scaled(241, 241)
+                self.label_4.setPixmap(self.pixmap)
 
 
 
